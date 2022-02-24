c3b3ac696999b46e24542f4bfa6b66f61ef4ed4a
sabo99 <49272368+sabo99@users.noreply.github.com>
2022-02-24 16:17:39 +0700

    fix middleware

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
 
